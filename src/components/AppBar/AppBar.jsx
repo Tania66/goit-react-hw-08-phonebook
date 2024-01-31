2c93c1a6de59f0824704c2aef822acbb90105895
Tania <113130993+Tania66@users.noreply.github.com>
2024-01-31 11:00:09 +0200

    add styled for contact

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,4 +1,4 @@
-import { Header, Img, Logo } from './AppBar.styled';
+import {  Header, Img, Logo} from './AppBar.styled';
 import Navigation from 'components/Navigation';
 import UserMenu from 'components/UserMenu/UserMenu';
 import AuthMenu from 'components/AuthMenu';
@@ -6,9 +6,11 @@ import { useSelector } from 'react-redux';
 import { isLogin } from '../../redux/auth/authSelector';
 import defaultAvatar from './default-phoneBook.png' 
 
+
 export const AppBar = () => {
   const login = useSelector(isLogin);
   const avatar = defaultAvatar;
+
   return (
     <Header>
       <Logo>
@@ -17,6 +19,7 @@ export const AppBar = () => {
       </Logo>
       
       {login ? <UserMenu /> : <AuthMenu />}
+
     </Header>
   );
 };
