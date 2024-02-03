10688e6693f7a73e82b37a48ab7d3b8b90aabc97
Tania <113130993+Tania66@users.noreply.github.com>
2024-02-03 18:13:47 +0200

    fix burger-menu

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,5 +1,4 @@
 import { Outlet } from 'react-router-dom';
-
 import { Suspense } from 'react';
 import { AppBar } from '../AppBar/AppBar';
 import { Container } from './Layout.styled';
@@ -8,9 +7,12 @@ export const Layout = () => {
   return (
     <Container>
       <AppBar />
+  
       <Suspense fallback={null}>
         <Outlet />
       </Suspense>
+    
+ 
     </Container>
   );
 };
